399e135de0c50f4efeb34281f275c6297dc32d28
Jens Kutilek <webmail@kutilek.de>
2026-03-09 18:08:33 +0100

    Copy font-level instructions

--- src/inst2ufo/copy.py
+++ src/inst2ufo/copy.py
@@ -11,7 +11,83 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
+LIB = "public.truetype.instructions"
+LIB_CVT = "controlValue"
+LIB_PREP = "controlValueProgram"
+LIB_FPGM = "fontProgram"
+
+
+def copy_cvt(font: TTFont, ufo: Font) -> None:
+    if "cvt " not in font:
+        logger.info("The font has no control value table (cvt).")
+        return
+    cvt = font["cvt "]
+    logger.info("Copying control value table")
+    ufo.lib[LIB][LIB_CVT] = {str(i): v for i, v in enumerate(cvt.values)}
+
+
+def copy_fpgm(font: TTFont, ufo: Font) -> None:
+    if "fpgm" not in font:
+        logger.info("The font has no font program table (fpgm).")
+        return
+    fpgm = font["fpgm"]
+    if not hasattr(fpgm, "program"):
+        logger.warning("The fpgm table does not contain a program.")
+        return
+    logger.info("Copying font program table")
+    ufo.lib[LIB][LIB_FPGM] = "\n".join(fpgm.program.getAssembly())
+
+
+def copy_glyf(font: TTFont, ufo: Font) -> None:
+    if "glyf" not in font:
+        logger.warning("The font has no glyf table.")
+        return
+
+
+def copy_maxp(font: TTFont, ufo: Font) -> None:
+    if "maxp" not in font:
+        logger.warning("The font has no maximum profile table (maxp).")
+        return
+    maxp = font["maxp"]
+    logger.info("Copying maximum profile table")
+    lib = ufo.lib[LIB]
+    lib["maxFunctionDefs"] = maxp.maxFunctionDefs
+    lib["maxInstructionDefs"] = maxp.maxInstructionDefs
+    lib["maxStackElements"] = maxp.maxStackElements
+    lib["maxStorage"] = maxp.maxStorage
+    lib["maxTwilightPoints"] = maxp.maxTwilightPoints
+    lib["maxZones"] = maxp.maxZones
+
+
+def copy_prep(font: TTFont, ufo: Font) -> None:
+    if "prep" not in font:
+        logger.info("The font has no control value program table (prep).")
+        return
+    prep = font["prep"]
+    if not hasattr(prep, "program"):
+        logger.warning("The prep table does not contain a program.")
+        return
+    logger.info("Copying control value program table")
+    ufo.lib[LIB][LIB_PREP] = "\n".join(prep.program.getAssembly())
+
+
 def copy_instructions(ttf_path: Path, ufo_path: Path, out_path: Path) -> None:
     font = TTFont(ttf_path)
     ufo = Font.open(ufo_path)
     logger.info(ufo)
+
+    if LIB in ufo.lib:
+        logger.warning("Overwriting existing TrueType instructions in UFO.")
+
+    ufo.lib[LIB] = {}
+
+    copy_cvt(font, ufo)
+    copy_fpgm(font, ufo)
+    copy_prep(font, ufo)
+    copy_maxp(font, ufo)
+    copy_glyf(font, ufo)
+
+    if ufo.lib[LIB]:
+        ufo.lib[LIB]["formatVersion"] = "1"
+
+    ufo.save(out_path, overwrite=True)
